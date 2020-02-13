e3cd7330327ad4bac71befc85a006bb0b0893134
agomezomg <anagomez98@unitec.edu>
2020-02-13 15:47:00 -0600

    i'm such a n00b

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace EntityFrameworkCRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.catgrid = new System.Windows.Forms.DataGridView();
+            this.btn_create = new System.Windows.Forms.Button();
+            this.btn_edit = new System.Windows.Forms.Button();
+            this.btn_del = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.txt_age = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.catgrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // catgrid
+            // 
+            this.catgrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.catgrid.Location = new System.Drawing.Point(12, 137);
+            this.catgrid.Name = "catgrid";
+            this.catgrid.Size = new System.Drawing.Size(468, 245);
+            this.catgrid.TabIndex = 0;
+            this.catgrid.MouseClick += new System.Windows.Forms.MouseEventHandler(this.catgrid_MouseClick);
+            // 
+            // btn_create
+            // 
+            this.btn_create.Location = new System.Drawing.Point(12, 108);
+            this.btn_create.Name = "btn_create";
+            this.btn_create.Size = new System.Drawing.Size(152, 23);
+            this.btn_create.TabIndex = 1;
+            this.btn_create.Text = "Create";
+            this.btn_create.UseVisualStyleBackColor = true;
+            this.btn_create.Click += new System.EventHandler(this.btn_create_Click);
+            // 
+            // btn_edit
+            // 
+            this.btn_edit.Location = new System.Drawing.Point(170, 108);
+            this.btn_edit.Name = "btn_edit";
+            this.btn_edit.Size = new System.Drawing.Size(152, 23);
+            this.btn_edit.TabIndex = 2;
+            this.btn_edit.Text = "Update";
+            this.btn_edit.UseVisualStyleBackColor = true;
+            this.btn_edit.Click += new System.EventHandler(this.btn_edit_Click);
+            // 
+            // btn_del
+            // 
+            this.btn_del.Location = new System.Drawing.Point(328, 108);
+            this.btn_del.Name = "btn_del";
+            this.btn_del.Size = new System.Drawing.Size(152, 23);
+            this.btn_del.TabIndex = 3;
+            this.btn_del.Text = "Delete";
+            this.btn_del.UseVisualStyleBackColor = true;
+            this.btn_del.Click += new System.EventHandler(this.btn_del_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(81, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(339, 36);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Entity Framework CRUD";
+            // 
+            // txt_id
+            // 
+            this.txt_id.Location = new System.Drawing.Point(64, 79);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(100, 20);
+            this.txt_id.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "CAT ID";
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(222, 79);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(100, 20);
+            this.txt_name.TabIndex = 8;
+            // 
+            // txt_age
+            // 
+            this.txt_age.Location = new System.Drawing.Point(380, 79);
+            this.txt_age.Name = "txt_age";
+            this.txt_age.Size = new System.Drawing.Size(100, 20);
+            this.txt_age.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(178, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "NAME";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(345, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "AGE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 394);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_age);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_id);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_del);
+            this.Controls.Add(this.btn_edit);
+            this.Controls.Add(this.btn_create);
+            this.Controls.Add(this.catgrid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.catgrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView catgrid;
+        private System.Windows.Forms.Button btn_create;
+        private System.Windows.Forms.Button btn_edit;
+        private System.Windows.Forms.Button btn_del;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.TextBox txt_age;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityFrameworkCRUD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CatBaseEntities context = new CatBaseEntities();
+            catgrid.DataSource = context.cats.ToList();
+        }
+
+        private void loadgrid()
+        {
+            CatBaseEntities context = new CatBaseEntities();
+            catgrid.DataSource = context.cats.ToList();
+        }
+
+        private void filltxtfields()
+        {
+            try
+            {
+                this.txt_id.Text = catgrid.SelectedRows[0].Cells[0].Value.ToString();
+                this.txt_name.Text = catgrid.SelectedRows[0].Cells[1].Value.ToString();
+                this.txt_age.Text = catgrid.SelectedRows[0].Cells[2].Value.ToString();
+            } catch (Exception e)
+            {
+                Console.WriteLine("could not load text");
+            }
+        }
+
+        private void btn_create_Click(object sender, EventArgs e)
+        {
+            using (CatBaseEntities context = new CatBaseEntities())
+            {
+                int nextID = context.cats.Last().catID;
+                cat mew_cat = new cat
+                {
+                    catID = nextID,
+                    catName = this.txt_name.Text,
+                    catAge = int.Parse(this.txt_age.Text),
+                    ownerID = 1
+                };
+                context.cats.Add(mew_cat);
+                context.SaveChanges();
+                loadgrid();
+            }
+
+        }
+
+        private void catgrid_MouseClick(object sender, MouseEventArgs e)
+        {
+            //Console.WriteLine("Location is:\nx = " + e.Location.X + "\ny = " + e.Location.Y);
+            filltxtfields();
+        }
+
+        private void btn_edit_Click(object sender, EventArgs e)
+        {
+            int key = int.Parse(this.txt_id.Text);
+            using (CatBaseEntities context = new CatBaseEntities())
+            {
+                cat mew_cat = context.cats.FirstOrDefault(input => input.catID==key);
+
+                mew_cat.catName = this.txt_name.Text;
+                mew_cat.catAge = int.Parse(this.txt_id.Text);
+                mew_cat.ownerID = 1;
+                
+                context.SaveChanges();
+                loadgrid();
+            }
+        }
+
+        private void btn_del_Click(object sender, EventArgs e)
+        {
+            int key = int.Parse(this.txt_id.Text);
+            using (CatBaseEntities context = new CatBaseEntities())
+            {
+                cat mew_cat = context.cats.FirstOrDefault(input => input.catID == key);
+
+                context.cats.Remove(mew_cat);
+                context.SaveChanges();
+                loadgrid();
+            }
+        }
+    }
+}
